26885b8e679d66f7f45d253ed30d33804053945f
Edward Aloba <ealoba@terpmail.umd.edu>
2021-04-11 13:08:01 -0400

    added chart to page

--- public/script.js
+++ public/script.js
@@ -1,15 +1,193 @@
-
-
 async function getData(){
-    const diningRequest = await fetch('/api/dining');
-    const diningData = await diningRequest.json;
+    const diningRequest = await fetch('/api/meals');
+    const diningData = await diningRequest.json();
+    //console.log(diningData);
+    return diningData;
+    console.log(diningData);
     
-    let tr = '';
-    let tableBody = document.querySelector('#table1');
-
-    diningData.data.forEach((restaurant) => {
-        let tr = document.createElement('tr');
-        tr.textContent = data.id
-        tableBody.appendChild(tr);
-    });
+  
 }
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }
+
+  async function windowActions(){
+    const meals = await getData();
+    
+    const mealArray = [1,2,3,4,5,6,7,8,9,10];
+    const selectedMeals = mealArray.map((element) => {
+        const random = getRandomIntInclusive(0,meals.length - 1);
+        return meals[random];
+    });
+
+    console.log(selectedMeals);
+}
+
+
+
+window.onload = function () {
+
+    async function getData(){
+        const diningRequest = await fetch('/api/meals');
+        const diningData = await diningRequest.json();
+        //console.log(diningData);
+        return diningData;
+        console.log(diningData);
+        
+      
+    }
+    
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+      }
+    
+      async function windowActions(){
+        const meals = await getData();
+        
+        const mealArray = [1,2,3,4,5,6,7,8,9,10];
+        const selectedMeals = mealArray.map((element) => {
+            const random = getRandomIntInclusive(0,meals.length - 1);
+            return meals[random];
+        });
+    
+        console.log(selectedMeals);
+    }
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        title:{
+            text: "Meals Vs Macros"
+        },
+        axisX: {
+            valueFormatString: "DDD"
+        },
+        axisY: {
+            prefix: "$"
+        },
+        toolTip: {
+            shared: true
+        },
+        legend:{
+            cursor: "pointer",
+            itemclick: toggleDataSeries
+        },
+        data: [{
+            type: "stackedBar",
+            name: "Meals",
+            showInLegend: "true",
+            xValueFormatString: "DD, MMM",
+            yValueFormatString: "$#,##0",
+            dataPoints: [
+                { x: new Date(2017, 0, 30), y: 56 },
+                { x: new Date(2017, 0, 31), y: 45 },
+                { x: new Date(2017, 1, 1), y: 71 },
+                { x: new Date(2017, 1, 2), y: 41 },
+                { x: new Date(2017, 1, 3), y: 60 },
+                { x: new Date(2017, 1, 4), y: 75 },
+                { x: new Date(2017, 1, 5), y: 98 }
+            ]
+        },
+        {
+            type: "stackedBar",
+            name: "Snacks",
+            showInLegend: "true",
+            xValueFormatString: "DD, MMM",
+            yValueFormatString: "$#,##0",
+            dataPoints: [
+                { x: new Date(2017, 0, 30), y: 86 },
+                { x: new Date(2017, 0, 31), y: 95 },
+                { x: new Date(2017, 1, 1), y: 71 },
+                { x: new Date(2017, 1, 2), y: 58 },
+                { x: new Date(2017, 1, 3), y: 60 },
+                { x: new Date(2017, 1, 4), y: 65 },
+                { x: new Date(2017, 1, 5), y: 89 }
+            ]
+        },
+        {
+            type: "stackedBar",
+            name: "Drinks",
+            showInLegend: "true",
+            xValueFormatString: "DD, MMM",
+            yValueFormatString: "$#,##0",
+            dataPoints: [
+                { x: new Date(2017, 0, 30), y: 48 },
+                { x: new Date(2017, 0, 31), y: 45 },
+                { x: new Date(2017, 1, 1), y: 41 },
+                { x: new Date(2017, 1, 2), y: 55 },
+                { x: new Date(2017, 1, 3), y: 80 },
+                { x: new Date(2017, 1, 4), y: 85 },
+                { x: new Date(2017, 1, 5), y: 83 }
+            ]
+        },
+        {
+            type: "stackedBar",
+            name: "Dessert",
+            showInLegend: "true",
+            xValueFormatString: "DD, MMM",
+            yValueFormatString: "$#,##0",
+            dataPoints: [
+                { x: new Date(2017, 0, 30), y: 61 },
+                { x: new Date(2017, 0, 31), y: 55 },
+                { x: new Date(2017, 1, 1), y: 61 },
+                { x: new Date(2017, 1, 2), y: 75 },
+                { x: new Date(2017, 1, 3), y: 80 },
+                { x: new Date(2017, 1, 4), y: 85 },
+                { x: new Date(2017, 1, 5), y: 105 }
+            ]
+        },
+        {
+            type: "stackedBar",
+            name: "Takeaway",
+            showInLegend: "true",
+            xValueFormatString: "DD, MMM",
+            yValueFormatString: "$#,##0",
+            dataPoints: [
+                { x: new Date(2017, 0, 30), y: 52 },
+                { x: new Date(2017, 0, 31), y: 55 },
+                { x: new Date(2017, 1, 1), y: 20 },
+                { x: new Date(2017, 1, 2), y: 35 },
+                { x: new Date(2017, 1, 3), y: 30 },
+                { x: new Date(2017, 1, 4), y: 45 },
+                { x: new Date(2017, 1, 5), y: 25 }
+            ]
+        }]
+    });
+    chart.render();
+    
+    function toggleDataSeries(e) {
+        if(typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+            e.dataSeries.visible = false;
+        }
+        else {
+            e.dataSeries.visible = true;
+        }
+        chart.render();
+    }
+    
+    }
+
+    
+
+var t = document.getElementById('table_id');
+var r =document.createElement('TR');
+t.tBodies[0].appendChild(r)
+
+
+
+getData();
+console.log('test');
+windowActions();
+//window.onload = windowActions;
+// let tr = '';
+// let tableBody = document.querySelector('#table_id');
+
+// diningData.data.forEach((restaurant) => {
+//     let tr = document.createElement('tr');
+//     tr.textContent = data.id
+//     tableBody.appendChild(tr);
+// });
